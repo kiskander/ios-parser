6ed923fb687e4558c68ff25f93f341cce0877b85
Kareem Iskander (kiskande) <kiskande@cisco.com>
2020-10-28 16:57:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cisco IOS Parser
+
+Simple script to parse IOS output
+
+## Getting Started
+
+```bash
+pip install -r requirements.txt
+```
+
+config is in `device.conf` file
+
---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from ciscoconfparse import CiscoConfParse
+from netaddr import *
+
+parse = CiscoConfParse('device.conf')
+
+# Return a list of all interfaces
+intfs = parse.find_objects(r"^interface")
+
+def translate_netmask(ip):
+    ip = IPNetwork(ip)
+    return ip.netmask
+
+# For each interface above, print out relevant information...
+for obj in intfs:
+    print("-----")
+
+    # Find the interface mode
+    intf_mode = obj.re_match_iter_typed(r'^\s*(switchport).*$', default='routed')
+
+    # Find the IP address by iterating over all children of this parent...
+    ip_addr = obj.re_match_iter_typed(r'ip\saddress\s(\S+)')
+
+    ip_mask = obj.re_match_iter_typed(r'ip\saddress\s\S+\s+(\S+)')
+
+    # Print out what we found...
+    print("Object: {0}".format(obj))
+    print("  Interface config line: {0}".format(obj.text))
+    print("  Interface mode: {0}".format(intf_mode))
+    print("  IP address: {0}".format(ip_addr))  # how do we replace the cidr with a netmask here
+    print("  IP mask: {0}".format(translate_netmask(ip_addr)))
+
+
